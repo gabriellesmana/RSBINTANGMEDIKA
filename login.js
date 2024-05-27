1d6d1d6f9b6e96c185c9b6429e1f42810c4c574f
gabriellesmana <mamypoko821@gmail.com>
2024-05-27 21:02:38 +0700

    Perbaikan tampilan dan tiket

--- login.js
+++ login.js
@@ -18,25 +18,7 @@ function validateLogin() {
     window.location.href = "HOME/home.html";
 }
 
-// JS GABISA MASUK SELAIN DEKSTOP //
 
-    // Function to check if device is desktop
-    function isDesktop() {
-      return !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-    }
-    // Function to display warning message if not desktop
-    function showDeviceWarning() {
-      if (!isDesktop()) {
-        // Menampilkan alert dengan pesan peringatan
-        alert("Maaf, perangkat Anda tidak mendukung. Harap gunakan PC/Laptop untuk pengalaman terbaik.");
-        // Menghentikan proses tampilan konten
-        document.body.style.display = 'none';
-      }
-    }
-    // Memanggil fungsi untuk menampilkan pesan peringatan saat halaman dimuat
-    showDeviceWarning();
-
-// JS GABISA MASUK SELAIN DEKSTOP //
 
 
 
